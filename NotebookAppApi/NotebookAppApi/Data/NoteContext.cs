02fec3e7d8f958c58e901605574d2321944d1fb8
Daniel <dagarhern@gmail.com>
2018-12-17 11:14:53 -0600

    Creation of NoteContext class

---
+++ NotebookAppApi/NotebookAppApi/Data/NoteContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotebookAppApi.Data
+{
+    public class NoteContext
+    {
+        private readonly IMongoDatabase _database = null;
+
+        public NoteContext(IOptions<Settings> settings)
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+            if (client != null)
+                _database = client.GetDatabase(settings.Value.Database);
+        }
+
+        public IMongoCollection<Note> Notes
+        {
+            get
+            {
+                return _database.GetCollection<Note>("Note");
+            }
+        }
+    }
+}
