d825a8fa048545420b753f54348d450828cb86bb
Daniel <dagarhern@gmail.com>
2018-12-17 11:23:01 -0600

    Note Repository update

--- NotebookAppApi/NotebookAppApi/Data/NoteRepository.cs
+++ NotebookAppApi/NotebookAppApi/Data/NoteRepository.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver.Linq;
 
 namespace NotebookAppApi.Data
 {
     public class NoteRepository
     {
+        Task<IEnumerable<Note>> GetAllNotes();
+        Task<Note> GetNote(string id);
+
+        //query after multiple parameters
+        Task<IEnumerable<Note>> GetNote(string bodyText, DateTime updateFrom, long headerSizeLimit);
+
+        //add new note document
+        
     }
 }
